28c90b2bf95e103399e61560978fcf413599d530
Michał Kruszewski <kruszewski2@gmail.com>
2020-12-08 15:54:46 +0100

    Save commit

--- sfservice.UI/Startup.cs
+++ sfservice.UI/Startup.cs
@@ -11,7 +11,6 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using sfservice.UI.API_Services;
-using sfservice.UI.Data;
 
 namespace sfservice.UI
 {
