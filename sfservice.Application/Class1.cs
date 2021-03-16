be04ead5285900a5d4cf0445813b57638f642397
Michał Kruszewski <kruszewski2@gmail.com>
2021-03-16 23:02:57 +0100

    Implementing clean architecture started

---
+++ sfservice.Application/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace sfservice.Application
+{
+    public class Class1
+    {
+    }
+}
---
+++ sfservice.Infrastructure/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace sfservice.Infrastructure
+{
+    public class Class1
+    {
+    }
+}
